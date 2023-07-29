ad886f29eb026bd5de418df7624719799399020c
bin-muhammad <binatiqmuhmmad26@gmail.com>
2023-07-29 14:37:32 -0400

    WORK OF JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,14 @@
+
+ alert("halo warld")
+var changeNameButton = document.getElementById("change_name_button")
+changeNameButton.addEventListener("click", changeName)
+
+function changeName() {
+    document.getElementById('mohammad').innerHTML = 'Bin'
+}
+document.write('<h1>halo warld</h1>')
+
+
+//let datatype work on his scope
+//var treat as a global varialbes
+
